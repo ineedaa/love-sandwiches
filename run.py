56b1772fa1e3158d3865448be8f896304a7a3691
Anitta Baby <anittab92@gmail.com>
2022-12-12 15:58:25 +0000

    Refactoring update worksheet function

--- run.py
+++ run.py
@@ -63,7 +63,12 @@ def update_surplus_worksheet(data):
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated")
-
+def update_worksheet(data,worksheet):
+    """Receives a list of integers to be inserted into a worksheet.Update the relevant worksheet with the data"""
+    print(f"Updating{worksheet} worksheet\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data) 
+    print(f"{worksheet} worksheet updated successfully\n") 
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stocks and  calculate the surplus data
@@ -81,9 +86,9 @@ def main():
     """ Run all programs""" 
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data,"sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data,"surplus")
 
 print("welcome")
 main()
