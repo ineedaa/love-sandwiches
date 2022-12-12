0b0dc9eae0538649cf72b994eb5e3b6f488e4852
Anitta Baby <anittab92@gmail.com>
2022-12-12 15:17:43 +0000

    updated calculate_surplus_data function to print last list

--- run.py
+++ run.py
@@ -55,8 +55,19 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated")
 
-
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
-
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stocks and  calculate the surplus data
+    """
+    print("Surplus data calculating\n")
+    stock=SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+def main():
+    """ Run all programs""" 
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+    print("welcome")
+main()
